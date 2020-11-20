5b189792197233c771ba1eafca8948308b6caf17
Raj <raj@laconik.com.au>
2020-11-20 09:10:44 +0800

    Updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {Layout, Breadcrumb} from 'antd';
+import {Avatar} from 'antd';
+import {MailOutlined, UserOutlined} from '@ant-design/icons';
+import Title from "antd/es/skeleton/Title";
+import {Menu, Button} from 'antd';
+
+const {SubMenu} = Menu;
+
+
+const {Header, Footer, Sider, Content} = Layout;
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Layout>
+                <Header style={{padding: 150}}>
+                    <h1 style={{float: "left", color: "White"}} level={3}> Laconik </h1>
+                    <Avatar style={{float: "right"}} src='./Chrome_Logo.png'/>
+                </Header>
+                <Layout>
+                    <Sider>
+                        <Menu mode={"inline"}>
+                            <Menu.Item key="Dashboard">
+                                Farm
+                            </Menu.Item>
+                            <SubMenu key="sub1"
+                                     title={
+                                         <span>
+              <MailOutlined/>
+              <span>Location List</span>
+            </span>
+                                     }>
+                                <Menu.ItemGroup title='About US'>
+                                    <Menu.Item key='Location 1'>Location 1</Menu.Item>
+
+                                    <Menu.Item key='Location 2'>Location 2</Menu.Item>
+                                </Menu.ItemGroup>
+                            </SubMenu>
+                        </Menu>
+                        Sider</Sider>
+                    <Layout>
+
+                        <Content className="site-layout-background"
+                                 style={{
+                                     padding: 200,
+                                     margin: 0,
+                                     minHeight: 580,
+                                 }}>
+                            <Breadcrumb style={{margin: '16px 0'}}>
+                                <Breadcrumb.Item>Dashboard</Breadcrumb.Item>
+                            </Breadcrumb>
+                            <div style={{background: '#fff', padding: 24, minHeight: 580}}
+                                 className="site-layout-content">Content
+                            </div>
+                        </Content>
+                        <Footer style = {{padding : 50 }}>footer</Footer>
+                    </Layout>
+                </Layout>
+            </Layout>
+        </div>
+    );
 }
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,14 +4,49 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//Store -> Globalised State
+
+//Action Increament
+
+/*const increment = () => {
+    return{
+        type : 'INCREMENT'
+    }
+}
+
+
+const DECREMENT = () => {
+    return{
+        type : 'DECREMENT'
+    }
+}
+
+
+//reducer
+const DECREMENT = () => {
+    return{
+        type : 'DECREMENT'
+    }
+}*/
+
+
+
+
+//dispatch
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,
+
+    document.getElementById('root')
 );
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
