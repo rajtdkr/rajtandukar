55b830f07bf77de79e434e6386a25508e5bf70b1
Raj <raj@laconik.com.au>
2020-11-23 13:31:43 +0800

    Updated

---
+++ src/containers/Header_app.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Header_app(){
+
+    return(  <div >
+         {/*//  <LoginOutlined />*/}
+            Laconik
+        </div>
+        )
+
+}
+export default Header_app;
